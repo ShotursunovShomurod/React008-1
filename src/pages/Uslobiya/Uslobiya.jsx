b87f828c03f7b8c71707b684cd13070961dc328c
ShotursunovShomurod <ziyodxon.i08@gmail.com>
2024-08-17 19:55:15 +0500

    qoshimcha qoshdim

--- src/pages/Uslobiya/Uslobiya.jsx
+++ src/pages/Uslobiya/Uslobiya.jsx
@@ -6,8 +6,8 @@ const Uslobiya = () => {
     const Navigate = useNavigate()
   return (
     <div className='container mx-auto  text-center'>
-        <img className='inline-block relative w-full mt-[40px] m-auto mb-[-150px]'  src={not} alt="" />
-        <button onClick={()=>Navigate("/")} className='px-[120px] absolute top-[834px] left-[735px] text-white font-bold py-[28px] bg-[rgb(0,120,219)] mt-[-00px] rounded-[40px]'>BACK TO HOME</button>
+        <img className='inline-block w-full h-full m-auto'  src={not} alt="" />
+        <button onClick={()=>Navigate("/")} className='px-[20px] text-white font-bold py-[14px] bg-[#2b00ff] mt-[-00px] rounded-[40px]'>BACK HOME</button>
     </div>
   )
 }
